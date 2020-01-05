37f9d547ce3d594fde1510dba50469b0751b36f3
Duviel Garcia <duviel7@gmail.com>
2020-01-05 11:57:37 -0500

    Se almacenan los datos del formulario en el state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,6 +2,23 @@ import React, { Fragment, useState } from 'react';
 
 
 function Formulario () {
+
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const actualizarState = e => {
+        
+        actualizarCita({
+            ...cita,
+            [e.target.name] : e.target.value
+        });
+    }
+
     return(
         <Fragment>
             <h2>Crear Cita</h2>
@@ -9,38 +26,43 @@ function Formulario () {
             <form>
                 <label>Nombre Mascota</label>
                 <input 
-                type="text" 
-                name="mascota"
-                className="u-full-width" 
-                placeholder="Nombre Mascota" 
+                    type="text" 
+                    name="mascota"
+                    className="u-full-width" 
+                    placeholder="Nombre Mascota"
+                    onChange={actualizarState}
                 />
 
                 <label>Nombre Dueño</label>
                 <input 
-                type="text" 
-                name="propietario"
-                className="u-full-width"  
-                placeholder="Nombre Dueño de la Mascota" 
+                    type="text" 
+                    name="propietario"
+                    className="u-full-width"  
+                    placeholder="Nombre Dueño de la Mascota"
+                    onChange={actualizarState}
                 />
 
                 <label>Fecha</label>
                 <input 
-                type="date" 
-                className="u-full-width"
-                name="fecha"
+                    type="date" 
+                    className="u-full-width"
+                    name="fecha"
+                    onChange={actualizarState}
                 />               
 
                 <label>Hora</label>
                 <input 
-                type="time" 
-                className="u-full-width"
-                name="hora" 
+                    type="time" 
+                    className="u-full-width"
+                    name="hora"
+                    onChange={actualizarState}
                 />
 
                 <label>Sintomas</label>
                 <textarea 
-                className="u-full-width"
-                name="sintomas"
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={actualizarState}
                 ></textarea>
 
                 <button type="submit" className="button-primary u-full-width">Agregar</button>
