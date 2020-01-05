be11cce8439b07afecedcd58c6651a6c4e11a050
Duviel Garcia <duviel7@gmail.com>
2020-01-05 11:21:52 -0500

    Componente principal y l formulario para crear las citas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+
 
 function App() {
+
+  const [citas, guardarCita] = useState([]);
+
+  console.log(citas);
+
   return (
-    <div>App</div>
+    <Fragment>
+      <h2>Administrador de pacientes</h2>
+      <div className="container">
+        <div className="rowq">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+
+          </div>
+        </div>  
+      </div>
+    </Fragment>
   );
 }
 
